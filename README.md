cc913067b27c2f94cab8739f5145e186fc2f8780
architect.bian <greatarchitect@163.com>
2018-08-31 20:22:38 +0800

    initialize demo code

---
+++ README.md
@@ -0,0 +1,54 @@
+﻿Windows客户端启动举例：
+
+1.设置host
+172.16.2.29			exadmin.exchain.com
+172.16.2.30			service.exchain.com
+#127.0.0.1			f.exadmin.exchain.com
+172.16.2.29			f.exadmin.exchain.com
+
+2.解压goadmin.tar.gz选择不要替换文件，运行goadmin.exe
+
+3.浏览器访问http://f.exadmin.exchain.com
+
+
+
+
+
+
+
+
+——————————————————————————————————————————————————————————————————————
+
+升级脚本举例：
+
+pkill goadmin
+cd /data/server/FinanceAdmin
+git pull
+
+tar -zvxf goadmin.tar.gz
+rm -rf views/login.tpl
+git checkout -- views/login.tpl
+setsid ./goadmin >> logs &
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Linux服务端部署举例：
+1.cd /data/server; git clone xxx
+1.设置nginx，参考nginx.conf配置文件
+2.配置Host指向：172.16.2.37 exadmin.exchain.com
+2.启动setsid ./goadmin >> logs/log &
---
+++ doPages/override.js
@@ -0,0 +1,37 @@
+/**
+ * 解析返回结果，每个项目规范不一致
+ * {
+"errorCode": 0,
+"errorMsg": "成功",
+"result": {}
+}
+ * @param data
+ *
+ * 解析为
+ * {
+ * success: true,
+ * err: 123,
+ * errmsg: "msg...",
+ * rows: []
+ * }
+ */
+function parseResult(data) {
+    console.log("parseResult(data):")
+    console.log(data)
+    var result = data
+    if (data.errorCode == 0) {
+        result.success = true
+    } else if (typeof data.errorCode != "undefined") {
+        result.success = false
+        result.msg = data.errorMsg
+    } else {
+        result.msg = "服务端返回结果不正确"
+    }
+    if (typeof data.result != "undefined") {
+        result.rows = data.result.data
+        result.total = data.result.total
+        result.adminuid = data.result.pn
+    }
+    console.log(result)
+    return result
+}
